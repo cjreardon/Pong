99dc0c65a561f724fb286fdfeeb3476a1faa2beb
cjreardon <connorreardon3@gmail.com>
2023-05-30 12:02:07 -0400

    Paddles stay on screen and show score

--- main.cpp
+++ main.cpp
@@ -1,30 +1,39 @@
 #include "raylib.h"
+#include <string> 
 
-struct Ball {
+struct Ball
+{
     float x, y;
     float speedX, speedY;
     float radius;
 
-    void Draw() {
-        DrawCircle(x, y, radius, WHITE); //center of window, draws from center of circle
+    void Draw()
+    {
+        DrawCircle(x, y, radius, WHITE); // center of window, draws from center of circle
     }
 };
 
-struct Paddle {
+struct Paddle
+{
     float x, y;
     float speed;
     float width, height;
+    int score;
+    bool flip;
 
-    Rectangle getRect() {
-        return Rectangle {x - width / 2, y - height / 2, width, height};
+    Rectangle getRect()
+    {
+        return Rectangle{x - width / 2, y - height / 2, width, height};
     }
 
-    void Draw() {
+    void Draw()
+    {
         DrawRectangleRec(getRect(), WHITE);
     }
 };
 
-int main() {
+int main()
+{
     InitWindow(800, 600, "Pong");
     SetWindowState(FLAG_VSYNC_HINT);
 
@@ -41,6 +50,8 @@ int main() {
     left.width = 10;
     left.height = 100;
     left.speed = 500;
+    left.score = 0;
+    left.flip = false;
 
     Paddle right;
     right.x = GetScreenWidth() - 50;
@@ -48,81 +59,128 @@ int main() {
     right.width = 10;
     right.height = 100;
     right.speed = 500;
+    right.score = 0;
+    right.flip = false;
 
-    const char* winnerText = nullptr;
+    std::string winnerText = "";
 
     while (!WindowShouldClose())
     {
 
         ball.x += ball.speedX * GetFrameTime();
         ball.y += ball.speedY * GetFrameTime();
-        if(ball.y < 0) {
+        if (ball.y < 0)
+        {
             ball.y = 0;
             ball.speedY *= -1;
         }
-        if(ball.y > GetScreenHeight()) {
+        if (ball.y > GetScreenHeight())
+        {
             ball.y = GetScreenHeight();
             ball.speedY *= -1;
         }
 
-        if(IsKeyDown(KEY_W)){
+        if (IsKeyDown(KEY_W))
+        {
             left.y -= left.speed * GetFrameTime();
         }
-        if(IsKeyDown(KEY_S)){
+        if (IsKeyDown(KEY_S))
+        {
             left.y += left.speed * GetFrameTime();
         }
 
-        if(IsKeyDown(KEY_UP)){
+        if (IsKeyDown(KEY_UP))
+        {
             right.y -= right.speed * GetFrameTime();
         }
-        if(IsKeyDown(KEY_DOWN)){
+        if (IsKeyDown(KEY_DOWN))
+        {
             right.y += right.speed * GetFrameTime();
         }
 
-        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, right.getRect())){
-            if(ball.speedX > 0){
+        if (left.y + left.height / 2 > GetScreenHeight())
+        {
+            left.y = GetScreenHeight() - left.height / 2;
+        }
+
+        if (right.y + right.height / 2 > GetScreenHeight())
+        {
+            right.y = GetScreenHeight() - right.height / 2;
+        }
+
+        if (left.y - left.height / 2 < 0)
+        {
+            left.y = left.height / 2;
+        }
+
+        if (right.y - right.height / 2 < 0)
+        {
+            right.y = right.height / 2;
+        }
+
+        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, right.getRect()))
+        {
+            if (ball.speedX > 0)
+            {
                 ball.speedX *= -1.1;
                 ball.speedY = (ball.y - right.y) / (right.height / 2) * -ball.speedX;
             }
         }
 
-        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, left.getRect())){
-            if(ball.speedX < 0){
+        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, left.getRect()))
+        {
+            if (ball.speedX < 0)
+            {
                 ball.speedX *= -1.1;
                 ball.speedY = (ball.y - left.y) / (left.height / 2) * ball.speedX;
             }
         }
 
-        if(ball.x < 0){
-            winnerText = "Right Player Wins!";
+        if (ball.x < 0 && !right.flip)
+        {
+            right.score += 1;
+            right.flip = true;
+            winnerText = "Right Player Wins! \n        " + std::to_string(left.score) + " - " + std::to_string(right.score);
         }
 
-        if(ball.x > GetScreenWidth()){
-            winnerText = "Left Player Wins!";
+        if (ball.x > GetScreenWidth() && !left.flip)
+        {
+            left.score += 1;
+            left.flip = true;
+            winnerText = "Left Player Wins! \n        " + std::to_string(left.score) + " - " + std::to_string(right.score);        
         }
-        if(winnerText && IsKeyPressed(KEY_SPACE)){
+
+        if (winnerText != "" && IsKeyPressed(KEY_SPACE))
+        {
             ball.x = GetScreenWidth() / 2;
             ball.y = GetScreenHeight() / 2;
             ball.speedX = 300;
             ball.speedY = 300;
-            winnerText = nullptr;
+            winnerText = "";
+            left.flip = false;
+            right.flip = false;
         }
 
         BeginDrawing();
-            ClearBackground(BLACK);
+        ClearBackground(BLACK);
 
-            ball.Draw();   
-            left.Draw();
-            right.Draw();         
-            
-            DrawFPS(10,10);
+        ball.Draw();
+        left.Draw();
+        right.Draw();
 
-            if(winnerText){
-                DrawText(winnerText, GetScreenWidth() / 2 - MeasureText(winnerText, 48) / 2, GetScreenHeight()/ 2, 48, GREEN);
+        DrawFPS(10, 10);
+
+        if (winnerText != "")
+        {
+            char* char_array = new char[winnerText.length() + 1];
+            char_array[winnerText.length()] = '\0';
+            for (int i = 0; i < winnerText.length(); i++) {
+                char_array[i] = winnerText[i];
             }
+            DrawText(char_array, GetScreenWidth() / 2 - MeasureText(char_array, 48) / 2, GetScreenHeight() / 2, 48, GREEN);
+        }
         EndDrawing();
     }
-    
 
     CloseWindow();
     return 0;
